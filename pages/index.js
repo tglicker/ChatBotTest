a6fbc6e49c937da061064cb3918ab6d86f3a7095
tglicker <31174980+tglicker@users.noreply.github.com>
2025-09-26 21:39:59 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 // pages/index.js
 import dynamic from 'next/dynamic';
 
-// Dynamically import ChatPrototype to disable SSR
-const ChatPrototype = dynamic(() => import('../components/ChatPrototype'), { ssr: false });
+// Dynamically import ChatPrototype to render only on the client
+const ChatPrototype = dynamic(
+  () => import('../components/ChatPrototype'),
+  { ssr: false, loading: () => <p>Loading chat...</p> }
+);
 
 export default function Home() {
   return <ChatPrototype />;
