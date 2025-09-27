2c05bfe406108e69f579714957151e5613660d0d
tglicker <31174980+tglicker@users.noreply.github.com>
2025-09-26 20:59:05 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,7 @@
+import "../styles/globals.css";
+import "carbon-components/css/carbon-components.min.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
 
