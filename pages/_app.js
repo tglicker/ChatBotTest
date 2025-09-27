6382fe5d745956c1f36fa40434aeaf08109f1940
tglicker <31174980+tglicker@users.noreply.github.com>
2025-09-26 21:39:33 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
+// pages/_app.js
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
