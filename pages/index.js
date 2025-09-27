d9534bbff4c07047a343955767a32b57edc73bd5
tglicker <31174980+tglicker@users.noreply.github.com>
2025-09-26 21:40:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-// pages/index.js
 import dynamic from 'next/dynamic';
 
-// Dynamically import ChatPrototype to render only on the client
 const ChatPrototype = dynamic(
   () => import('../components/ChatPrototype'),
   { ssr: false, loading: () => <p>Loading chat...</p> }
